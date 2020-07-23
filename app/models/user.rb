e356f93284a4d7e880d1b2717eef724ef43f4506
lawa0921 <bag571ivy3470@gmail>
2020-07-23 14:23:43 +0800

    翻譯檔調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: ["member", "teacher", "admin"]
+  validates :name, presence: true, uniqueness: true
+  validates :role, presence: true
 end
