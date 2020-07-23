d90f6e909af32a229ae00117bc9ff35e204075dd
lawa0921 <bag571ivy3470@gmail>
2020-07-23 21:45:02 +0800

    course edit and update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,14 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:edit, :update]
+
   def index
     @courses = Course.all
   end
+
   def new
     @course = Course.new
   end
+
   def create
     @course = Course.new(course_params)
     @course.user = current_user
@@ -16,8 +20,24 @@ class CoursesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to courses_path, notice: t('courses.update')
+    else
+      flash[:notice] = t('courses.fail')
+      render :edit
+    end
+  end
+
   private
   def course_params
     params.require(:course).permit(:name, :description, :price, :currency, :type_of_course, :public, :url, :valididy_period)
+  end
+
+  def set_course
+    @course = current_user.courses.find(params[:id])
   end
 end
