8cb462261c35fc008fb5de42dea6b29baff63bdb
lawa0921 <bag571ivy3470@gmail>
2020-07-23 11:33:46 +0800

    增加欄位位置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: ["member", "teacher", "admin"]
 end
