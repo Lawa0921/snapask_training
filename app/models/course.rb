c54d961272986fea84b3c24384201a15607420bd
lawa0921 <bag571ivy3470@gmail>
2020-07-24 14:00:41 +0800

    scope fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,4 @@ class Course < ApplicationRecord
   enum type_of_course: ["math", "english", "japanese", "chinese", "social", "science", "history", "other"]
   validates :name, :price, :currency, :type_of_course, :public, :valididy_period, presence: true
   scope :open_public, -> { where( public: true ) }
-  scope :unexpired, -> { where( ["valididy_period > ? ", DateTime.now ] ) }
-  scope :expired, -> { where( ["valididy_period < ?", DateTime.now ] ) }
 end
