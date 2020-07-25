fc85e982b0691d0c65e391f5d2107e8225952700
lawa0921 <bag571ivy3470@gmail>
2020-07-25 14:18:54 +0800

    user feature spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,4 +55,12 @@ RSpec.configure do |config|
   config.after(:all) do
     DatabaseCleaner.clean
   end
+
+  private
+  def user_login(user)
+    visit new_user_session_path
+    fill_in("user[email]", with: "#{user.email}")
+    fill_in("user[password]", with: "#{user.password}")
+    click_button (I18n.t("users.log_in"))
+  end
 end
