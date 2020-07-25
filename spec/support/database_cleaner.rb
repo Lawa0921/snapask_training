ebfd92af1687613458b599890abd8dcda3131178
lawa0921 <bag571ivy3470@gmail>
2020-07-25 11:39:36 +0800

    set database-cleaner

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,22 @@
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+end
