05889e31f4e74b2ca8e6303b5f85ce93b1e2aa9b
lawa0921 <bag571ivy3470@gmail>
2020-07-26 13:04:49 +0800

    api_v0 helper and exception

--- app/api/api_v0/base.rb
+++ app/api/api_v0/base.rb
@@ -1,7 +1,8 @@
 module ApiV0
   class Base < Grape::API
     version 'v0', using: :path
-
+    helpers ::ApiV0::Helpers
+    include ApiV0::ExceptionHandlers
     use ApiV0::Auth::Middleware
 
     mount Ping
--- app/api/api_v0/ping.rb
+++ app/api/api_v0/ping.rb
@@ -2,7 +2,7 @@ module ApiV0
   class Ping < Grape::API
     desc 'Ping pong'
     get "/ping" do
-      "pong"
+      authenticate!
     end
   end
 end
