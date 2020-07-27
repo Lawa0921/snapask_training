68010b3f0726ff977529baeefc0c20338cbc9c69
lawa0921 <bag571ivy3470@gmail>
2020-07-27 12:16:56 +0800

    增加購買的測試

--- spec/features/carts_spec.rb
+++ spec/features/carts_spec.rb
@@ -34,4 +34,30 @@ RSpec.feature "Carts", type: :feature do
       end
     end
   end
+  describe "When checkout cart" do
+    before do
+      user_login(member_user)
+    end
+    context "when add two courses" do
+      before do
+        courses
+        visit root_path
+        find(:css, ".main-card-link:first-child").click
+        click_link I18n.t("add_to_cart")
+        find(:css, ".main-card-link:nth-child(2)").click
+        click_link I18n.t("add_to_cart")
+        click_link "#{I18n.t("courses.cart")} 2"
+        click_link I18n.t("checkout")
+      end
+      it "should be success" do
+        expect(page).to have_content I18n.t("courses.purchased_course_notice")
+      end
+      it "should be have two purchased_courses" do
+        click_link I18n.t("users.purchased_courses")
+        within('.container') do
+          expect(all('.main-card-link').count).to eq 2
+        end
+      end
+    end
+  end
 end
