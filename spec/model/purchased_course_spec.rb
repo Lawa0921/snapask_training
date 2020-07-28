039baa4d359661f49347c2a6c7f5b81207c8d57b
lawa0921 <bag571ivy3470@gmail>
2020-07-28 22:07:18 +0800

    purchased_course 測試小調整

--- spec/model/purchased_course_spec.rb
+++ spec/model/purchased_course_spec.rb
@@ -5,8 +5,6 @@ RSpec.feature "Purchased course", type: :model do
   let (:course) { create(:course, user_id: teacher_user.id) }
   let (:unpublic_course) { create(:course, user_id: teacher_user.id, public: false) }
   let (:success_purchased_course) { create(:purchased_course, user_id: member_user.id, course_id: course.id) }
-  let (:owner_fail_purchased_course) { create(:purchased_course, user_id: teacher_user.id, course_id: course.id) }
-  let (:public_fail_purchased_course) { create(:purchased_course, user_id: member_user.id, course_id: unpublic_course.id) }
   describe "model validation" do
     context "可以正常建立" do
       it "when 正常填入" do
