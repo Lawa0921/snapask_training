2cf370fb5228570022c3017c050bcb1acc48d00c
lawa0921 <bag571ivy3470@gmail>
2020-07-29 16:59:57 +0800

    expand api create purchased course spec

--- spec/api/api_v0/purchased_course_spec.rb
+++ spec/api/api_v0/purchased_course_spec.rb
@@ -3,8 +3,8 @@ RSpec.feature "Purchased course", type: :request do
   let (:member_user) { create(:user, role: 0 ) }
   let (:teacher_user) { create(:user, role: 1 ) }
   let (:teachers_course) { create(:course, name: "teachers_course", user_id: teacher_user.id ) }
-  let (:courses) {create_list(:course, 10, user_id: teacher_user.id)}
-  let (:purchased_course) { create(:purchased_course, user_id: member_user.id, course_id: teachers_course.id) }
+  let (:unpublic_course) { create(:course, name: "unpublic_course", user_id: teacher_user.id, public: false ) }
+  let (:purchased_course) { create(:purchased_course, user_id: member_user.id, course_id: teachers_course.id ) }
   describe "POST" do
     context "/api/v0/purchased_courses" do
       before do
@@ -42,6 +42,19 @@ RSpec.feature "Purchased course", type: :request do
           purchased_course
           access_key = member_user.api_access_token
           post "/api/v0/purchased_courses", params: { access_key: access_key, course_id: @course_id}
+          @result = JSON.parse(response.body)
+        end
+        it "should be create failed" do
+          expect(response).to be_a_bad_request
+          expect(response.status).to eq(400)
+          expect(@result["message"]).to eq("400 Purchased course fail")
+        end
+      end
+
+      context "when 購買未開放課程" do
+        before do
+          access_key = member_user.api_access_token
+          post "/api/v0/purchased_courses", params: { access_key: access_key, course_id: unpublic_course.id}
           @result = JSON.parse(response.body)
         end
         it "should be create failed" do
